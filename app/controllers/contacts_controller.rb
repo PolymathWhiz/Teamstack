1ba6ab69b555468910f49bbf4e7c412103d6a321
Miracle Anyanwu <miracleanyanwu@PolymathWhiz.local>
2017-05-29 14:41:20 +0100

    still working on contacts design

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,21 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Thank you for contacting us"
+      redirect_back fallback_location: contact_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def contact_params
+      params.require(:contact).permit(:name, :email, :subject, :message)
+    end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,15 @@
 class Contact < ApplicationRecord
+  before_save :downcase_email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: false, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :subject, presence: true, length: { maximum: 50 }
+  validates :message, presence: true, length: { maximum: 200 }
+
+  private 
+    def downcase_email 
+      email.downcase!
+    end
 end
