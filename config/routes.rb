04e9067329810444acd852f67e555ac82bc7777e
Miracle Anyanwu <miracleanyanwu@PolymathWhiz.local>
2017-05-29 12:09:32 +0100

    making things ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'contacts/new'
+
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/privacy'
+
+  get 'pages/terms'
+
+  get 'pages/information'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
