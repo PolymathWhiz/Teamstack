5a3d7aa2359f2749f9a79314e3e54bc46ff031fa
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2017-05-31 04:51:08 +0100

    about trying to fix the height difference of the faq header when running on mobile

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,13 +3,13 @@ class Contact < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: false, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
   validates :subject, presence: true, length: { maximum: 50 }
   validates :message, presence: true, length: { maximum: 200 }
 
-  private 
-    def downcase_email 
+  private
+    def downcase_email
       email.downcase!
     end
 end
