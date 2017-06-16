056da98f2884efe588de1ac05a8ff09c60939062
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2017-06-16 21:42:07 +0100

    locations view okay

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,7 @@ module ApplicationHelper
   def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: current_user.email + " gravatar", width: 130, height: 80, 
+                            class: "img-responsive img-rounded")
   end
 end
