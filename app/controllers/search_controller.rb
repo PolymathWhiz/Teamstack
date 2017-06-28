f5308ef39f3ee6e01d94264713a61ab612d1635e
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2017-06-29 00:33:08 +0100

    Search controller works perfectly! Name changed too

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,16 +2,16 @@ class SearchController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    search = params[:search].present? ? params[:search] : ""
-    where = {}
-    where[:city] = params[:adv].capitalize if params[:adv].present?
+    search = params[:search]
     @term = search
-
-    @search = 
-      if search
-        User.search(search, fields: [:first_name, :state, :country], misspellings: false, where: { city: where[:city] } )
-      end
-
+    city = params[:city]
+    where = {}
+    where[:city] = city.capitalize unless !city.present?
+    if search.present? && city.present?
+        @search = User.search(search, fields: [:first_name, :state, :country], misspellings: false, where: { city: where[:city] } )
+    else
+       @search = User.search(search, fields: [:first_name, :state, :country], misspellings: false)
+    end
     @total = @search.total_count
     @time = @search.took
     @search = Kaminari.paginate_array(@search).page(params[:page]).per(10) # Paginates the results
