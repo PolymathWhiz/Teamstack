03d220ff440431578c03954659742a51b41af444
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2017-06-29 16:06:34 +0100

    making progress with the skill

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,10 @@ module UsersHelper
   def is_current_user?(user)
     user == current_user
   end
+
+  def skill_select
+    ['Web Developer', 'Marketer', 'Designer', 'Mobile Developer', 'VR Developer', 'Investor', 'Other']
+  end
   
   def avaliability
     [ ['Avaliable for work', true], ['Not avaliable for work', false] ]
