359dcca46ec380f6ffc4b520c268b043ec8f5578
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2017-07-10 09:27:37 +0100

    making progress

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,3 +1,8 @@
 class Admin::HomeController < Admin::BaseController
-  def index; end
-end
+  def index
+    @user_count = User.all.count
+    @total_feedback = Feedback.all.count
+    @total_contact_messages = Contact.all.count
+    @total_admins = User.where(admin: true).count
+  end
+end 
